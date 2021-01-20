415289318962558f5d19745f5a5bc139b61b64fe
andrew beatrice <abeatrice.mail@gmail.com>
2021-01-19 16:56:30 -0800

    add cors to api gateway and list recipes function

--- listRecipes/app.js
+++ listRecipes/app.js
@@ -1,18 +1,6 @@
 const {DynamoDBClient, ScanCommand} = require("@aws-sdk/client-dynamodb");
 let response;
 
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
- */
 exports.lambdaHandler = async (event, context) => {
     try {
         const client = new DynamoDBClient({region: "us-east-1"});
@@ -27,9 +15,14 @@ exports.lambdaHandler = async (event, context) => {
             }
         });
         response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                data: items,
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Headers" : "Content-Type",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET"
+            },
+            body: JSON.stringify({
+                data: items
             })
         }
     } catch (err) {
