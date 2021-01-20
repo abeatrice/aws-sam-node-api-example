ceb1feaf1661ef7e809ca705168e63bc5c5f2776
andrew beatrice <abeatrice.mail@gmail.com>
2021-01-20 15:14:52 -0800

    validate create recipe

--- createRecipe/app.js
+++ createRecipe/app.js
@@ -1,8 +1,23 @@
 const {DynamoDBClient, PutItemCommand} = require("@aws-sdk/client-dynamodb");
 const {v4: uuidv4} = require("uuid");
+const Joi = require("joi")
 
-exports.lambdaHandler = async (event, context) => {
+exports.lambdaHandler = async (event) => {
+
+    // validate request body
     const body = JSON.parse(event.body);
+    const schema = Joi.object({
+        Name: Joi.string().required(),
+        Description: Joi.string().required()
+    });
+    const { error, value } = schema.validate(body);
+    if(error !== undefined) {
+        return {
+            statusCode: 400,
+            body: error.message
+        }
+    }
+
     const recipe = {
         ID: {S: uuidv4()},
         Name: {S: body.Name},
@@ -19,8 +34,8 @@ exports.lambdaHandler = async (event, context) => {
         return error;
     }
     return {
-        'statusCode': 201,
-        'body': JSON.stringify({
+        statusCode: 201,
+        body: JSON.stringify({
             data: {
                 ID: recipe.ID.S,
                 Name: body.Name,
